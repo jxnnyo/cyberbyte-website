c751ad57246bf1bc0d0c4952b7d24e79319993fa
Sam Collins <sam@scollins.co>
2021-05-06 02:28:53 +0100

    Added security headers

---
+++ next.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*?)",
+        headers: [
+          {
+            key: 'Referrer-Policy',
+            value: 'no-referrer'
+          }, {
+            key: 'X-Content-Type-Options',
+            value: 'X-Content-Type-Options: nosniff'
+          }, {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          }, {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self' 'unsafe-inline' fonts.googleapis.com fonts.gstatic.com data:; connect-src 'self' formspree.io; form-action formspree.io;"
+          },
+        ]
+      }
+    ]
+  }
+}
