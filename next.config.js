f73708a9598acc7ac8c9394fc70c49a6f9c94cfe
Sam Collins <sam@scollins.co>
2022-01-10 13:50:48 +0000

    Cleaned up CSS

--- next.config.js
+++ next.config.js
@@ -24,6 +24,34 @@ module.exports = withBundleAnalyzer({
             key: "X-Frame-Options",
             value: "SAMEORIGIN",
           },
+          {
+            key: "Report-To",
+            value: '{"group":"default","max_age":10886400,"endpoints":[{"url":"https://cb-software.uriports.com/reports"}],"include_subdomains":true}',
+          },
+          {
+            key: "Reporting-Endpoints",
+            value: 'default="https://cb-software.uriports.com/reports"',
+          },
+          {
+            key: "NEL",
+            value: '{"report_to":"default","max_age":2592000,"include_subdomains":true,"failure_fraction":1.0}',
+          },
+          {
+            key: "Content-Security-Policy-Report-Only",
+            value: "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-ancestors 'self'; report-uri https://cb-software.uriports.com/reports/report; report-to default",
+          },
+          {
+            key: "Permissions-Policy-Report-Only",
+            value: 'accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()',
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy-Report-Only",
+            value: 'require-corp; report-to="default"',
+          },
+          {
+            key: "Cross-Origin-Opener-Policy-Report-Only",
+            value: 'same-origin; report-to="default"',
+          }
         ],
       },
     ];
