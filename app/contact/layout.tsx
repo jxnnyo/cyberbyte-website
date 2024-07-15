5e22af31b9b30bc99fe4fd2df077128a27eac746
Sam Collins <sam@cyberbyte.software>
2024-07-15 11:40:23 +0100

    Added recapture for bots

--- app/contact/layout.tsx
+++ app/contact/layout.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contact Us",
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,14 +1,47 @@
+"use client";
+
 import React from "react";
-import type { Metadata } from "next";
-import {contactForm} from "../../services/contactForm";
+import { useForm, ValidationError } from "@formspree/react";
+import {
+  GoogleReCaptchaProvider,
+  useGoogleReCaptcha,
+} from "react-google-recaptcha-v3";
+import Link from "next/link";
 
-export const metadata: Metadata = {
-  title: "Contact Us",
-};
+function ContactForm() {
+  const { executeRecaptcha } = useGoogleReCaptcha();
 
-function Contact() {
+  const [state, handleSubmit] = useForm("mqkwewrv", {
+    data: { "g-recaptcha-response": executeRecaptcha },
+  });
+
+  if (state.succeeded) {
+    return (
+      <section>
+        <div className="mx-auto max-w-6xl px-4 sm:px-6">
+          <div className="bg-gray-800 py-10 px-8 md:py-16 md:px-12">
+            <div className="flex flex-col items-center justify-between lg:flex-row">
+              <div className="mb-6 text-center lg:mr-16 lg:mb-0 lg:w-3/4 lg:text-left">
+                <h3 className="h3 mb-2 text-white">Thanks for your message</h3>
+                <p className="text-lg text-gray-400">
+                  We will get back to you shortly.
+                </p>
+              </div>
+
+              <div className="flex w-full justify-center lg:w-1/4 lg:justify-end">
+                <Link href="/" className="btn btn-primary">
+                  Return Home
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+
   return (
-    <form className="mx-auto max-w-3xl" action={contactForm}>
+    <form className="mx-auto max-w-3xl" onSubmit={handleSubmit}>
       <div className="-mx-3 mb-4 flex flex-wrap">
         <div className="w-full px-3">
           <label
@@ -88,9 +121,26 @@ function Contact() {
           >
             Send
           </button>
+          <span className="text-gray-400">
+            This site is protected by reCAPTCHA and the Google{" "}
+            <a href="https://policies.google.com/privacy">Privacy Policy</a>
+            {" and "}
+            <a href="https://policies.google.com/terms">
+              Terms of Service
+            </a>{" "}
+            apply.
+          </span>
         </div>
       </div>
     </form>
+  );
+}
+
+function Contact() {
+  return (
+    <GoogleReCaptchaProvider reCaptchaKey="6LfBWxAqAAAAAPyJJ1mr3FyRN44Xn_OdsnN0gAzI">
+      <ContactForm />
+    </GoogleReCaptchaProvider>
   );
 }
 
